65d26f9118487abaf3abfbf8a3d8ecace898c5af
hardik <hardiksandip@gmail.com>
2020-05-28 23:12:48 +0530

    add comments

--- generate.php
+++ generate.php
@@ -1,22 +1,27 @@
 <?php
-
+// start session
 session_start();
-
+// specify output to image/jpeg
 header("Content-type: image/jpeg");
 
+// get text from session to print on captcha image
 $text = $_SESSION['secure'];
 
+// set font size
 $font_size = 30;
 
+// image height and width
 $image_height = 40;
 $image_width = 150;
 
+// create blank image with specific height and width
 $image = imagecreate($image_width, $image_height);
-
+// set image background color to white
 imagecolorallocate($image, 255, 255, 255);
-
+// set text color to black
 $text_color = imagecolorallocate($image, 0, 0, 0);
 
+// generate random lines on captcha image
 for($x = 1 ;$x <= 30 ;$x++)
 {
     $x1 = rand(1,100);
@@ -26,9 +31,10 @@ for($x = 1 ;$x <= 30 ;$x++)
     imageline($image, $x1, $y1, $x2, $y2, $text_color);
 }
 
+// set font style, font text color, font size and captcha text on image
 imagettftext($image, $font_size, 0, 15, 30, $text_color, 'myfont.ttf', $text);
 
+// generate captcha jpeg image
 imagejpeg($image);
-
 ?>
 
--- index.php
+++ index.php
@@ -4,16 +4,18 @@
     
     if(!isset($_POST['secure']))
     {
+        // generate captcha text and store on session
         $_SESSION['secure'] = rand(1000, 9999);
     }
     else
     {
         if($_SESSION['secure'] == $_POST['secure'])
         {
-            echo "match";
+            echo "captcha match";
         }
         else
         {
+            // if captcha incorrect than generate another captcha text and store on session
             echo "incorrect captcha";
             $_SESSION['secure'] = rand(1000, 9999);
         }
@@ -27,6 +29,7 @@
     <title></title>
 </head>
 <body>
+    <!-- Display captcha image from session stored value-->
     <img src="generate.php" />
     <form action="index.php" method="POST">
         type the value you see: <input type="text" name="secure" />
